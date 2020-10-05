3e721db902fa89653f39ccf8d346ef120e163cd8
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2020-10-05 08:42:52 +0700

    init praktikum vr6

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame(string page) {
+        SceneManager.LoadScene(page);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
