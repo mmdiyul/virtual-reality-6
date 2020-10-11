d36d4ce36f4431b001182d6803fd3a61dd04c396
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2020-10-11 18:38:06 +0700

    update level scene & menu script

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public Text textField;
+
     public void PlayGame(string page) {
         SceneManager.LoadScene(page);
     }
 
     public void Exit() {
         Application.Quit();
+    }
+
+    public void SetText(string text) {
+        textField.text = text;
     }
 }
